e8b10a3f6c03033b3953e97a2dfdfcd5d6d7a8a5
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-06 22:25:56 +0800

    social software list

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -13,8 +13,19 @@ const Home = () => {
 
       <h2>開發契機</h2>
       <p>
-        某天我去用了舊的靠北南工匿名發言發現乾不能傳圖片上去還有圖文樣式永遠只有一個黑底白字不能改變QQ所以就跑來自幹一個靠北網頁。
+        某天我去用了舊的靠北南工匿名發言發現
+        <br />
+        乾不能傳圖片上去還有圖文樣式永遠只有一個黑底白字不能改變QQ
+        <br />
+        所以就跑來自幹一個靠北網頁。
       </p>
+
+      <h2>支援發布社群平台</h2>
+      <div className="social-software">
+        <i className="fab fa-discord"></i>
+        <i className="fab fa-twitter-square"></i>
+      </div>
+
     </div>
   );
 };
