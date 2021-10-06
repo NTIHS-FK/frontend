d5a1e5b327824ddba7b028414accbcdbba63e824
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-06 20:46:48 +0800

    edit title and add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './components/home/home';
 
 const App = () => {
   return (
-    <div></div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './app.sass';
 
 ReactDOM.render(
     <React.StrictMode>
