08f66724846933b7394840278462c29e14e95eb7
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-24 20:04:00 +0800

    sign up form change value

--- src/components/form/sign-up/index.tsx
+++ src/components/form/sign-up/index.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import TextField from '@mui/material/TextField';
+import {SignUpType} from './type';
 import './signUp.sass';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState<SignUpType>({
+    name: '',
+    email: '',
+    password: '',
+    confirm: '',
+  });
+
+  const changeValue = (key: string) => {
+    return (event: ChangeEvent) => {
+      const value = (event.target as HTMLInputElement).value;
+      setFormData({
+        name: key === 'name' ? value : formData.name,
+        email: key === 'email' ? value : formData.email,
+        password: key === 'password' ? value : formData.password,
+        confirm: key === 'confirm' ? value : formData.confirm,
+      });
+    };
+  };
+
   return (
     <div id="sign-up">
       <h2>註冊</h2>
@@ -10,16 +30,28 @@ const SignUp = () => {
         label="Name"
         variant="standard"
         sx={{width: '300px'}}
+        onChange={changeValue('name')}
       />
       <TextField
         label="Email"
         variant="standard"
         sx={{width: '300px'}}
+        onChange={changeValue('email')}
       />
       <TextField
         label="Password"
         variant="standard"
         sx={{width: '300px'}}
+        inputProps={{type: 'password'}}
+        onChange={changeValue('password')}
+      />
+      <TextField
+        label="Confirm"
+        variant="standard"
+        sx={{width: '300px'}}
+        inputProps={{type: 'password'}}
+        onChange={changeValue('confirm')}
+        error={formData.password !== formData.confirm}
       />
     </div>
   );
---
+++ src/components/form/sign-up/type.ts
@@ -0,0 +1,6 @@
+export interface SignUpType {
+  name: string;
+  email: string;
+  password: string;
+  confirm: string;
+}
