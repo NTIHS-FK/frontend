757e4a6d6da0adae6de841fb316591dba833f64a
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-24 19:18:27 +0800

    add isgn up form

--- src/components/form/sign-up/index.tsx
+++ src/components/form/sign-up/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
 import './signUp.sass';
 
 const SignUp = () => {
   return (
     <div id="sign-up">
+      <h2>註冊</h2>
+      <TextField
+        label="Name"
+        variant="standard"
+        sx={{width: '300px'}}
+      />
+      <TextField
+        label="Email"
+        variant="standard"
+        sx={{width: '300px'}}
+      />
+      <TextField
+        label="Password"
+        variant="standard"
+        sx={{width: '300px'}}
+      />
     </div>
   );
 };
